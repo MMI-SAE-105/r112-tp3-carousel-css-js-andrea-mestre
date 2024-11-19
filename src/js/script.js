0facb80e7471acf280c5a85ec533e62afa5d44fa
andrea-mestre <andrea.mestre@edu.univ-fcomte.fr>
2024-11-19 16:41:39 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,17 @@ const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 
+
+
 if (carousel) {
     prevButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: -260, behavior: "smooth" }); 
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" }); 
     });
 
-    nextButton.addEventListener("click", () =>{
-        carousel.scrollBy({ left: +260, behavior: "smooth" });
+    nextButton.addEventListener("click", () => {
+        carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" }); 
     });
+    const premierItem = document.querySelector(".carousel__item");
+    const scrollAmount = premierItem.clientWidth;
 }
+
